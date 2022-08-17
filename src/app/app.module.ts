14074efb3a5e4a17577d3d31d86f64e33367a7fc
Prajwal P <prajwal.praveen@u-next.com>
2022-08-17 20:48:30 +0530

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SafePipeModule } from './safe.pipe.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SafePipeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
